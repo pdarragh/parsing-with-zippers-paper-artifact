674de15b84d3d81362d6d54c4f647b45e7942633
Pierce Darragh <pierce.darragh@gmail.com>
2020-07-02 00:33:58 -0600

    Adjust appendix code for column width

--- appendix/pwz_driver.ml
+++ appendix/pwz_driver.ml
@@ -4,24 +4,21 @@ open Pwz_derive
 
 let init_zipper (e : exp) : zipper =
   let e'    = Seq (s_bottom, []) in
-  let m_top = { start_pos = p_bottom; parents = [TopC];
-                end_pos = p_bottom; result = e_bottom } in
+  let m_top = { start_pos = p_bottom; parents = [TopC]; end_pos = p_bottom; result = e_bottom } in
   let c     = SeqC (m_top, s_bottom, [], [e; { m = m_bottom; e' = Tok t_eof }]) in
-  let m_seq = { start_pos = p_bottom; parents = [c];
-                end_pos = p_bottom; result = e_bottom } in
+  let m_seq = { start_pos = p_bottom; parents = [c]; end_pos = p_bottom; result = e_bottom } in
   (e', m_seq)
 
 let unwrap_top_zipper ((e', m) : zipper) : exp =
   match m.parents with
   | [SeqC ({ parents = [TopC] }, s_bottom, [e; _], [])] -> e
-  | _ -> failwith "Invalid top zipper."
+  | _ -> failwith "Invalid top zipper"
 
 let parse (ts : tok list) (e : exp) : exp list =
   let rec parse' (p : pos) (ts : tok list) (z : zipper) : zipper list =
     match ts with
     | []       -> derive p t_eof z
-    | t :: ts' -> List.concat (List.map (fun z' -> parse' (ref (!p + 1)) ts' z')
-                                 (derive p t z)) in
+    | t :: ts' -> List.concat (List.map (fun z' -> parse' (ref (!p + 1)) ts' z') (derive p t z)) in
   List.map unwrap_top_zipper (parse' (ref 0) ts (init_zipper e))
 
 type ast = Ast of sym * ast list
